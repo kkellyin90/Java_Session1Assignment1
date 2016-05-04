27bf5eb713e88103dffb7da1ba3586338e893127
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-04 12:17:50 +0530

    Added files via upload

---
+++ sumOfNumbers.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Keith
+ *
+ */
+public class sumOfNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int integernum = 5;
+		int sumOfIntegers;
+		float floatnum = 35.5f;
+		// sum of integernum and floatnum(converted to int)	is stored in sumOfIntegers	
+		sumOfIntegers = integernum + (int)floatnum;
+		System.out.println("The sum of 2 integers is " + sumOfIntegers);
+
+	}
+
+}
